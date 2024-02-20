c37342dc791e1731600084b0069f15d6e75525eb
Renn7571 <157162784+Renn7571@users.noreply.github.com>
2024-02-20 15:04:01 -0400

    Add files via upload

---
+++ ProjetoBlog/ProjetoBlog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from blog import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('<slug:slug>/', views.post_details, name='post_details')
+    # path('login/', views.user_login, name='login'),
+]
